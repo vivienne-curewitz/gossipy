851c7ee3c1cb2e5a967ff88dd241955dd7f4f332
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-12-07 12:29:36 +0100

    average worker sort of works with random walks

--- gossip_process/gossip_node.py
+++ gossip_process/gossip_node.py
@@ -3,70 +3,174 @@
 # 2. Listener - waits for gossip from a friend
 # 3. Chatter - sends gossip to a friend
 # obviously they aren't friends, just sister processes which form the nodes that are walked by the learning algorithm.
-from threading import Thread, Queue
+from threading import Thread 
+from queue import Queue, Empty
 import requests
 from random import randint
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from multiprocessing import Process
+from worker import MeanWorker
 
 # god python is so bad at networking; wtf
 class PeerHandler(BaseHTTPRequestHandler):
-    parent = None
-
-    def __init__(self, parent):
-        self.parent = parent
-    def do_GET(self):
-        if self.path == "/ping":
+    def do_POST(self):
+        if self.path == "/peers":
+            # parse
+            clength = int(self.headers.get("Content-Length", 0))
+            data = self.rfile.read(clength)
+            print(f"Got peer request: {str(data)}")
+            jdata = json.loads(data)
+            self.server.node.add_peer(jdata["address"])
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b"pong")
-        elif self.path == "/status":
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            payload = {"ok": True, "id": "peer123"}
-            self.wfile.write(json.dumps(payload).encode())
+            self.wfile.write(json.dumps({"peers": self.server.node.peers}).encode())
         else:
             self.send_response(404)
             self.end_headers()
+
+    def do_PUT(self):
+        if self.path == "/data":
+            self.send_response(200)
+            self.end_headers()
+            clength = int(self.headers.get("Content-Length", 0))
+            data = self.rfile.read(clength)
+            self.server.node.handle_data_put(data)
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+class PeerHttpServer(HTTPServer):
+    def __init__(self, server_address, RequestHandlerClass, node):
+        super().__init__(server_address, RequestHandlerClass)
+        self.node = node  # <--- shared state
 
 class GossipNode:
     name: str
     ip: str
     port: int
     models: list
-    chatter: Thread
-    worker: Thread
     inqueue: Queue
     outqueue: Queue
     max_peers: int
-    num_peers_to_share: int
+    worker: MeanWorker
+    # num_peers_to_share: int
     peers: list[str] 
-    def __init__(self, name: str, ip: str, port: int, max_peers=10, num_peers_to_share = 5):
+    def __init__(self, name: str, ip: str, port: int, worker, max_peers=10):
         self.name = name
         self.ip = ip
         self.port = port
         self.inqueue = Queue()
         self.outqueue = Queue()
         self.max_peers = max_peers
-        self.num_peers_to_share = num_peers_to_share
+        # self.num_peers_to_share = num_peers_to_share
+        self.peers = []
+        self.worker = worker
+        self.worker.inqueue = self.inqueue
+        self.worker.outqueue = self.outqueue
+
+    def start(self):
+        # start other threads
+        chatter = Thread(target=self.send, daemon=True) 
+        worker_thread = Thread(target=self.worker.run)
+        # start listen thread
+        # a bit hacky, but this is the case when it has been seeded with a base peer
+        if len(self.peers) == 1:
+            self.get_peers()
+        chatter.start()
+        worker_thread.start()
+        self.listen()
+
+    def get_peers(self):
+        print(f"{self.name} getting peers")
+        resp = requests.post(f"http://{self.peers[0]}/peers", json={"address": f"{self.ip}:{self.port}"}, timeout=1)
+        jdata = resp.json()
+        self.add_many_peers(jdata["peers"])
     
+    # always adds the peer
+    def add_peer(self, peer):
+        if peer in self.peers:
+            return
+        if len(self.peers) >= self.max_peers:
+            # random replace
+            self.peers[randint(0, len(self.peers) - 1)] = peer
+        else:
+            self.peers.append(peer)
+
+    def add_many_peers(self, peers: list[str]):
+        self.peers += peers
+        # i know this sucks
+        self.peers = list(set(self.peers))
+        while len(self.peers) > self.max_peers:
+           rem_ind = randint(0, len(self.peers) - 1) 
+           self.peers.pop(rem_ind)
+
     def listen(self):
-        ph = PeerHandler(self)
-        server = HTTPServer((self.url, self.port), ph)
+        server = PeerHttpServer((self.ip, self.port), PeerHandler, self)
         print("Server running")
         server.serve_forever()
 
+    def get_random_peer(self):
+        if len(self.peers) < 1:
+            return None
+        if len(self.peers) == 1:
+            return self.peers[0]
+        pi = randint(0, len(self.peers) - 1)
+        return self.peers[pi]
+
     def send(self):
-        model_params = self.outqueue.get()
-        num_peers = len(self.peers)
-        peer = self.peers[self.peers[randint(num_peers)]]
-        if num_peers > self.num_peers_to_share:
-            send_peers = [randint(num_peers) for x in range(max(self.num_peers_to_share))]
-        else:
-            send_peers = self.peers
-        send_data = {
-            "peers": send_peers,
-            "model": model_params
-        }
-        requests.put(f"{peer}new", json.dumps(send_data))
+        while True:
+            try:
+                model_params = self.outqueue.get(timeout=1)
+            except Empty as e:
+                model_params = self.worker.data
+            print(f"{self.name} -- Peers: {self.peers}")
+            peer = self.get_random_peer()
+            if peer is None:
+                return
+            send_data = {
+                "peers": self.peers,
+                "model": model_params
+            }
+            requests.put(f"http://{peer}/data", json.dumps(send_data))
+
+    def handle_data_put(self, data):
+        # data is encode json
+        jdata = json.loads(data)
+        peers = jdata["peers"]
+        model = jdata["model"]
+        self.add_many_peers(peers)
+        self.inqueue.put(model)
+
+def node_process(local_ip, base_port, i):
+    print(f"Start Node at {local_ip}:{base_port+i}")
+    wt = MeanWorker(i+1)
+    gt = GossipNode(f"gp{i}", local_ip, base_port+i, wt)
+    gt.add_peer(f"{local_ip}:{base_port}")
+    gt.start()
+
+def node_base_process(local_ip, base_port, i):
+    print(f"Start Node at {local_ip}:{base_port+i}")
+    wt = MeanWorker(i)
+    gt = GossipNode(f"gp{i}", local_ip, base_port+i, wt)
+    gt.start()
+
+def run_many(gns: int):
+    procs: list[Process] = []
+    base_port = 8080
+    local_ip = "0.0.0.0"
+    w0 = MeanWorker(0)
+    # gn.start()
+    procs.append(Process(target=node_base_process, args=[local_ip, base_port, 0], daemon=True))
+    for i in range(gns - 1):
+        procs.append(Process(target=node_process, args=[local_ip, base_port, i+1], daemon=True))
+    for proc in procs:
+        proc.start()
+    for proc in procs:
+        proc.join()
+
+
+if __name__ == "__main__":
+    # gn = GossipNode("base", "0.0.0.0", 8080)
+    # gn.start()
+    run_many(10)
